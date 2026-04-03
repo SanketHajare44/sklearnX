731dcd0480fbebffcf6af326e95071f4bcc94551
SanketHajare44 <sankethajare44@gmail.com>
2026-04-03 23:14:41 +0530

    Implement KNN regressor using neighbor averaging

--- sklearnX/neighborsX/KNeighborsRegressorX.py
+++ sklearnX/neighborsX/KNeighborsRegressorX.py
@@ -4,6 +4,17 @@ import numpy as np
 
 class KNeighborsRegressorX(KNeighborsBase):
 
+    ###################################################################
+    #
+    #   Function Name : predict
+    #   Input         : Testing data
+    #   Output        : Predicted data
+    #   Description   : It is used to predict
+    #   Author        : Sanket Sadashiv Hajare
+    #   Date          : 03//04/2026
+    #
+    ###################################################################
+
     def predict(self,X):
 
         X = np.array(X)
