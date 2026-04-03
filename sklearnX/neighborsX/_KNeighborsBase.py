2b32e7ca60c1fbf4e5a5e5e5060a8265da3d0ad5
SanketHajare44 <sankethajare44@gmail.com>
2026-04-03 22:43:36 +0530

    Add KNN base class for shared distance computation logic

---
+++ sklearnX/neighborsX/_KNeighborsBase.py
@@ -0,0 +1,101 @@
+import numpy as np 
+
+class KNeighborsBase:
+    def __init__(self, n_neighbors=5, metric='euclidean'):
+        self.n_neighbors = n_neighbors
+        self.metric = metric
+        self._X_train = None
+        self._Y_train = None
+
+    def fit(self, X ,Y):
+        X = np.array(X, dtype=float)
+        Y = np.array(Y)
+
+        # checks the dimensions
+        if(X.ndim != 2):
+            raise ValueError("X must be a 2D array")
+        
+        # Check Rows of the feature and colmun
+        if(len(X) != len(Y)):
+            raise ValueError("rows of X and Y required same")
+
+        if(self.n_neighbors > len(X)):
+            raise ValueError(f"n_neighbors {self.n_neighbors} is greater than the number of training samples ")
+        
+        self._X_train = X 
+        self._Y_train = Y 
+
+        return self
+    
+    def _check_is_fitted(self):
+        if( self._X_train is None):
+            raise ValueError("Model is not fit, call the first fit")
+    
+    def _euclidean_distance(self,P1,P2):
+
+        total = 0
+
+        for i in range(len(P1)):
+
+            total = total + ((P1[i] - P2[i])**2)
+        
+        return np.sqrt(total)
+
+    def _manhattan_distance(self,P1,P2):
+
+        total = 0
+
+        for i in range(len(P1)):
+
+            total = total + abs(P1[i] - P2[i])
+        
+        return total
+
+    def _distance(self,P2):
+        
+        distance = []
+
+        # if(self.metric == 'euclidean'):
+            
+        #     for i in range(len(self._Y_train)):
+        #         dist = self._euclidean_distance(self._X_train[i],P2)
+        #         distance.append((dist,self._Y_train[i]))
+        
+        # elif(self.metric == 'manhattan'):
+            
+        #     for i in range(len(self._Y_train)):
+        #         dist = self._manhattan_distance(self._X_train[i],P2)
+        #         distance.append((dist,self._Y_train[i]))
+        
+        # else:
+        #     raise ValueError("Unknown metric, Use euclidean/manhattan")
+
+        # return distance
+
+        for i in  range(len(self._X_train)):
+ 
+            if(self.metric == 'euclidean'):
+                dist = self._euclidean_distance(self._X_train[i],P2)
+                distance.append((dist,self._Y_train[i]))
+            
+            elif(self.metric == 'manhattan'):
+                dist = self._manhattan_distance(self._X_train[i],P2)
+                distance.append((dist,self._Y_train[i]))
+            
+            else:
+                raise ValueError("Unknown metric, use euclidean / manhattan")
+            
+            
+        return distance
+    
+    def _get_n_neighbors(self,P2):
+        
+        self._check_is_fitted()
+
+        distance = self._distance(P2)
+
+        sorted_data = sorted(distance, key=lambda item : item[0])
+
+        return sorted_data[:self.n_neighbors]
+        
+        
